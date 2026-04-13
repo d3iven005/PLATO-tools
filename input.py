d48a332945f730f3d8348df2380441e04fb6749f
J1VV3N YU <63585164+d3iven005@users.noreply.github.com>
2026-04-13 17:46:10 +0100

    Add files via upload

--- input.py
+++ input.py
@@ -1,12 +1,13 @@
-job = 2   ### 1 for molecular orbital calculation, 2 for electron localisation function
-energylevel = '1,2,4-6'   ## If job = 1, choose the orbital you want to calculate
-jobname = 'h2oy'
-ncpu = 8
+job = 1   ### 1 for molecular orbital calculation, 2 for electron localisation function
+energylevel = '37,38'   ## If job = 1, choose the orbital you want to calculate
+jobname = 'Caffeine'
+ncpu = 12
 ifcrystal = 0  ## 0 for non-periodic, 1 for crystal
-vectorflag = 0 ## 0 for reading from xyz file, 1 for setting up by your self
+padding_distance = 3 ## The box boundary
+vectorflag = 1 ## 0 for reading from xyz file, 1 for setting up by your self
 vectorA = [20, 0, 0]
 vectorB = [0, 20, 0]
 vectorC = [0, 0, 20]  ##unit: Bohr = 0.529177 Angstrom
-N1 = 100
-N2 = 100
-N3 = 100  ### number of grids for three vectors
+N1 = 150
+N2 = 150
+N3 = 150  ### number of grids for three vectors
